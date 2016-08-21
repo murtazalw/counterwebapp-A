49a97ecf9ef6d74a64a5394ab22c156a548ef432
murtazalw <murtaza_lw@yahoo.com>
2016-08-21 14:54:13 +0300

    Revert "m1"
    This reverts commit 1de037c9059cc06c8ea7d7484c11598e8ef02e6c.

--- com/mkyong/controller/MergeTest.java
+++ com/mkyong/controller/MergeTest.java
@@ -7,6 +7,9 @@ public class MergeTest {
 
 	public static void main(String[] args) {
 
-		System.out.println("m1");
+		System.out.println("m1-1");
+		System.out.println("m1");		
+		System.out.println("m2");		
+		System.out.println("m3");
 	}
 }
