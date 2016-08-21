caaf6e29fb7457bfadc12194051cb0cc703b99d4
murtazalw <murtaza_lw@yahoo.com>
2016-08-21 15:02:16 +0300

    b1

--- com/mkyong/controller/MergeTest.java
+++ com/mkyong/controller/MergeTest.java
@@ -10,8 +10,7 @@ public class MergeTest {
 		System.out.println("m1-1");
 		System.out.println("m1");		
 		System.out.println("m2");		
-		System.out.println("m3");
-		
+		System.out.println("m3");		
 		System.out.println("b1");
 	}
 }
